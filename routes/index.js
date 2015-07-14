15de5a273e01de6c71120d1889d4cd29ff7ba8db
Eli saul Florez Perez <elisaul77@gmail.com>
2015-07-13 23:25:21 -0500

    moderacion de cometarios

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/author', function(req, res) {
   res.render('author',{errors:[]});
 });
 router.param('quizId',quizController.load);
+router.param('commentId',commentController.load);
 
 
 router.get('/login',sessionController.new);
@@ -30,4 +31,5 @@ router.delete('/quizes/:quizId(\\d+)',sessionController.loginRequired,quizContro
 
 router.get('/quizes/:quizId(\\d+)/comments/new',commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',sessionController.loginRequired,commentController.publish);
 module.exports = router;
